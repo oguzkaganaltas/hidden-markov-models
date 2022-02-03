47213efcc67cd3308ece2e4fccae674a8272ed45
oguz <oguzkaganaltas@gmail.com>
2022-02-03 17:12:35 +0200

    fixed

--- hmm.py
+++ hmm.py
@@ -14,9 +14,9 @@ def forward(A, B, pi, O):
     :return: The probability of the observation sequence and the calculated alphas in the Trellis diagram with shape
              (N, T) which should be a numpy array.
     """
-    trellis_diagram = np.zeros((2,len(O)))
+    trellis_diagram = np.zeros((len(pi),len(O)))
     for obsr in range(len(O)):
-        for i in range(2):
+        for i in range(len(pi)):
             if obsr == 0:
                 trellis_diagram[i][obsr] = pi[i] * B[i][O[obsr]]
             else:
@@ -37,17 +37,17 @@ def viterbi(A, B, pi, O):
     :return: The most likely state sequence with shape (T,) and the calculated deltas in the Trellis diagram with shape
              (N, T). They should be numpy arrays.
     """
-    trellis_diagram = np.zeros((2,len(O)))
+    trellis_diagram = np.zeros((len(pi),len(O)))
     most_probable_state_sequence = []
     for obsr in range(len(O)):
         if obsr == 0:
-            for i in range(2):
+            for i in range(len(pi)):
                 trellis_diagram[i][obsr] = pi[i] * B[i][O[obsr]]
             path = np.argmax(trellis_diagram[:,obsr])
             most_probable_state_sequence.append(path)
         else:
             path = np.argmax(trellis_diagram[:,obsr-1])
-            trellis_diagram[path,obsr] = trellis_diagram[path][obsr-1] * A[path][path]*B[path][O[obsr]]
-            trellis_diagram[int(not path),obsr] = trellis_diagram[path][obsr-1] * A[path][int(not path)]*B[int(not path)][O[obsr]]
+            for j in range(len(pi)):
+                trellis_diagram[j][obsr] = trellis_diagram[path][obsr-1] * A[path][j] * B[j][O[obsr]] 
             most_probable_state_sequence.append(np.argmax(trellis_diagram[:,obsr]))
     return most_probable_state_sequence, trellis_diagram

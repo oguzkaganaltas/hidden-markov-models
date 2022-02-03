c06a8ac6567c402ae2ab555dc7eb7aef1ab60615
oguz <oguzkaganaltas@gmail.com>
2022-02-03 10:09:53 +0200

    init

---
+++ hmm.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def forward(A, B, pi, O):
+    """
+    Calculates the probability of an observation sequence O given the model(A, B, pi).
+    :param A: state transition probabilities (NxN)
+    :param B: observation probabilites (NxM)
+    :param pi: initial state probabilities (N)
+    :param O: sequence of observations(T) where observations are just indices for the columns of B (0-indexed)
+        N is the number of states,
+        M is the number of possible observations, and
+        T is the sequence length.
+    :return: The probability of the observation sequence and the calculated alphas in the Trellis diagram with shape
+             (N, T) which should be a numpy array.
+    """
+
+
+def viterbi(A, B, pi, O):
+    """
+    Calculates the most likely state sequence given model(A, B, pi) and observation sequence.
+    :param A: state transition probabilities (NxN)
+    :param B: observation probabilites (NxM)
+    :param pi: initial state probabilities(N)
+    :param O: sequence of observations(T) where observations are just indices for the columns of B (0-indexed)
+        N is the number of states,
+        M is the number of possible observations, and
+        T is the sequence length.
+    :return: The most likely state sequence with shape (T,) and the calculated deltas in the Trellis diagram with shape
+             (N, T). They should be numpy arrays.
+    """
